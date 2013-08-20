0ad840d5cf235b64498d17cc842a996d6bf1e976
Draco Li <draco@dracoli.com>
2013-08-19 23:07:31 -0400

    Initial Commit

---
+++ sunny/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sunny
+//
+//  Created by Draco on 2013-08-19.
+//  Copyright Draco 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
